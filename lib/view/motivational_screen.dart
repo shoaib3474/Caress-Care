11ca0f69c7b67b5e2a95dcc48fd8f4f036f1c495
tayyab0378 <tayyab0378@gmail.com>
2025-05-28 17:08:37 +0500

    added background in motivation screen

--- lib/view/motivational_screen.dart
+++ lib/view/motivational_screen.dart
@@ -1,3 +1,4 @@
+import 'package:caress_care/utils/const/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class MotivationalScreen extends StatelessWidget {
@@ -5,6 +6,19 @@ class MotivationalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: AppColors.mainGradient,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text('M O T I V A T I O N A L S C R E E N'),
+      ),
+    );
   }
 }
