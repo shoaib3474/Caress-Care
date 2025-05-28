fe5c52409b22fb7b60622fbe82d3cfddd5ad1bb5
tayyab0378 <tayyab0378@gmail.com>
2025-05-28 17:05:26 +0500

    change in app routes

--- lib/view/motivational_screen.dart
+++ lib/view/motivational_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class MotivationalScreen extends StatelessWidget {
-  const MotivationalScreen({Key? key}) : super(key: key);
+  const MotivationalScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
